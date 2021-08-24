5577160e0b54a86ad29d98a1878e7d9636f89bd8
dhthang1998 <dhthang1998@gmail.com>
2021-08-24 16:21:34 +0700

    Add campaign repository

---
+++ IMP/IMP.Application/Interfaces/Repositories/ICampaignRepositoryAsync.cs
@@ -0,0 +1,11 @@
+﻿using IMP.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IMP.Application.Interfaces.Repositories
+{
+    public interface ICampaignRepositoryAsync : IGenericRepositoryAsync<Campaign>
+    {
+    }
+}
---
+++ IMP/IMP.Infrastructure.Persistence/Repositories/CampaignRepositoryAsync.cs
@@ -0,0 +1,20 @@
+﻿using IMP.Application.Interfaces.Repositories;
+using IMP.Domain.Entities;
+using IMP.Infrastructure.Persistence.Contexts;
+using IMP.Infrastructure.Persistence.Repository;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IMP.Infrastructure.Persistence.Repositories
+{
+    public class CampaignRepositoryAsync : GenericRepositoryAsync<Campaign>, ICampaignRepositoryAsync
+    {
+        private readonly DbSet<Campaign> _campaigns;
+        public CampaignRepositoryAsync(ApplicationDbContext dbContext) : base(dbContext)
+        {
+            _campaigns = dbContext.Set<Campaign>();
+        }
+    }
+}
