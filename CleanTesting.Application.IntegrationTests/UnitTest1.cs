21fb817569080b22076675b36d765ddc3caf38bd
dhthang1998 <dhthang1998@gmail.com>
2021-09-07 01:32:23 +0700

    Comment test

--- CleanTesting.Application.IntegrationTests/UnitTest1.cs
+++ CleanTesting.Application.IntegrationTests/UnitTest1.cs
@@ -1,29 +1,29 @@
-using FluentAssertions;
-using IMP.Application.DTOs.ViewModels;
-using IMP.Application.Features.Platforms.Queries.GetAllPlatforms;
-using IMP.Application.Wrappers;
-using NUnit.Framework;
-using System.Collections;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-namespace CleanTesting.Application.IntegrationTests
-{
-    using static Testing;
-    public class Tests
-    {
-        [SetUp]
-        public void Setup()
-        {
-        }
+//using FluentAssertions;
+//using IMP.Application.DTOs.ViewModels;
+//using IMP.Application.Features.Platforms.Queries.GetAllPlatforms;
+//using IMP.Application.Wrappers;
+//using NUnit.Framework;
+//using System.Collections;
+//using System.Collections.Generic;
+//using System.Threading.Tasks;
+//namespace CleanTesting.Application.IntegrationTests
+//{
+//    using static Testing;
+//    public class Tests
+//    {
+//        [SetUp]
+//        public void Setup()
+//        {
+//        }
 
-        [Test]
-        public async Task ShouldReturnAllListPlatforms()
-        {
-            var query = new GetAllPlatformsQuery();
+//        [Test]
+//        public async Task ShouldReturnAllListPlatforms()
+//        {
+//            var query = new GetAllPlatformsQuery();
 
-            var result = await SendAsync<PagedResponse<IEnumerable<PlatformViewModel>>>(query);
+//            var result = await SendAsync<PagedResponse<IEnumerable<PlatformViewModel>>>(query);
 
-            result.Data.Should().HaveCountGreaterThan(0);
-        }
-    }
-}
+//            result.Data.Should().HaveCountGreaterThan(0);
+//        }
+//    }
+//}
