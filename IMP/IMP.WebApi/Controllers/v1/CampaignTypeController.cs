0f70193480a9a374250e7b3b78d64e4f2422e054
danghieuthang <dhthang1998@gmail.com>
2021-09-20 23:25:25 +0700

    Add authorize for CUD campaign type

--- IMP/IMP.WebApi/Controllers/v1/CampaignTypeController.cs
+++ IMP/IMP.WebApi/Controllers/v1/CampaignTypeController.cs
@@ -4,6 +4,7 @@ using IMP.Application.Features.CampaignTypes.Commands.DeleteCampaignTypeById;
 using IMP.Application.Features.CampaignTypes.Commands.UpdateCampaignType;
 using IMP.Application.Features.CampaignTypes.Queries;
 using IMP.Application.Features.CampaignTypes.Queries.GetCampaignTypeById;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace IMP.WebApi.Controllers.v1
@@ -13,6 +14,7 @@ namespace IMP.WebApi.Controllers.v1
     public class CampaignTypeController : BaseApiController
     {
         [HttpPost]
+        [Authorize(Roles = "Administrator")]
         public async Task<IActionResult> Create([FromForm] CreateCampaignTypeCommand command)
         {
             return StatusCode(201, await Mediator.Send(command));
@@ -31,7 +33,8 @@ namespace IMP.WebApi.Controllers.v1
         }
 
         [HttpPut("{id}")]
-        public async Task<IActionResult> Update([FromRoute]int id,[FromForm] UpdateCampaignTypeCommand command)
+        [Authorize(Roles = "Administrator")]
+        public async Task<IActionResult> Update([FromRoute] int id, [FromForm] UpdateCampaignTypeCommand command)
         {
             if (command.Id != id)
             {
@@ -41,6 +44,7 @@ namespace IMP.WebApi.Controllers.v1
         }
 
         [HttpDelete("{id}")]
+        [Authorize(Roles = "Administrator")]
         public async Task<IActionResult> Delete([FromRoute] int id)
         {
             return Ok(await Mediator.Send(new DeleteCampaignTypeByIdCommand { Id = id }));
