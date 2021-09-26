6b34653fee8a2ce7ef6ebd3d7c085042a8bfbc23
danghieuthang <dhthang1998@gmail.com>
2021-09-26 20:48:30 +0700

    Add filter by date for Get wallet transaction

--- IMP/IMP.Application/Features/WalletTransactions/Queries/GetTransactionsByWalletId/GetWalletTransactionByWalletIdQuery.cs
+++ IMP/IMP.Application/Features/WalletTransactions/Queries/GetTransactionsByWalletId/GetWalletTransactionByWalletIdQuery.cs
@@ -5,6 +5,7 @@ using IMP.Application.Models;
 using IMP.Application.Models.ViewModels;
 using IMP.Application.Wrappers;
 using IMP.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,6 +18,10 @@ namespace IMP.Application.Features.WalletTransactions.Queries.GetTransactionsByW
     public class GetWalletTransactionByWalletIdQuery : PageRequest, IListQuery<WalletTransactionViewModel>
     {
         private int _applicationUserId;
+        [FromQuery(Name = "from-date")]
+        public DateTime? FromDate { get; set; }
+        [FromQuery(Name = "to-date")]
+        public DateTime? ToDate { get; set; }
 
         public void SetApplicationUserId(int walletId)
         {
@@ -33,7 +38,9 @@ namespace IMP.Application.Features.WalletTransactions.Queries.GetTransactionsByW
 
             public override async Task<Response<IPagedList<WalletTransactionViewModel>>> Handle(GetWalletTransactionByWalletIdQuery request, CancellationToken cancellationToken)
             {
-                var page = await _walletTransactionRepository.GetPagedList(predicate: x => x.Wallet.ApplicationUserId == request._applicationUserId,
+                var page = await _walletTransactionRepository.GetPagedList(predicate: x => x.Wallet.ApplicationUserId == request._applicationUserId
+                    && (!request.FromDate.HasValue || x.Created.Date >= request.FromDate.Value.Date)
+                    && (!request.ToDate.HasValue || x.Created.Date <= request.ToDate.Value.Date),
                         orderBy: request.OrderField,
                         orderByDecensing: request.OrderBy == OrderBy.DESC,
                         pageIndex: request.PageIndex,
