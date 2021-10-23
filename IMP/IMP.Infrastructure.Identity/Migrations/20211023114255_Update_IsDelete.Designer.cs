61e323c7a27766c4671f4abb7c092776d6669a07
danghieuthang <dhthang1998@gmail.com>
2021-10-23 18:43:59 +0700

    Fixed migrate db in identity context

--- IMP/IMP.Infrastructure.Identity/Migrations/20211023114255_Update_IsDelete.Designer.cs
+++ IMP/IMP.Infrastructure.Identity/Migrations/20211023114255_Update_IsDelete.Designer.cs
@@ -4,14 +4,16 @@ using IMP.Infrastructure.Identity.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace IMP.Infrastructure.Identity.Migrations
 {
     [DbContext(typeof(IdentityContext))]
-    partial class IdentityContextModelSnapshot : ModelSnapshot
+    [Migration("20211023114255_Update_IsDelete")]
+    partial class Update_IsDelete
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ IMP/IMP.Infrastructure.Identity/Migrations/20211023114255_Update_IsDelete.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace IMP.Infrastructure.Identity.Migrations
+{
+    public partial class Update_IsDelete : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IsDelete",
+                schema: "Identity",
+                table: "RefreshTokens",
+                newName: "IsDeleted");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IsDeleted",
+                schema: "Identity",
+                table: "RefreshTokens",
+                newName: "IsDelete");
+        }
+    }
+}
