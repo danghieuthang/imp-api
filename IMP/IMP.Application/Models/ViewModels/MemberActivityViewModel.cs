d79a6fb34768a96efcbcf171f818bbd6fab4d24a
Dang Hieu Thang <dthhang1998@gmail.com>
2021-11-24 14:04:22 +0700

    Update actuvutty

--- IMP/IMP.Application/Models/ViewModels/MemberActivityViewModel.cs
+++ IMP/IMP.Application/Models/ViewModels/MemberActivityViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using IMP.Application.Models.Compaign;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +9,7 @@ namespace IMP.Application.Models.ViewModels
 {
     public class MemberActivityViewModel : BaseViewModel<int>
     {
-        public int CampaignActivityId { get; set; }
+        public CampaignActivityViewModel CampaignActivity { get; set; }
         public int CampaignMemberId { get; set; }
         public int Status { get; set; }
         public List<ActivityCommentViewModel> ActivityComments { get; set; }
