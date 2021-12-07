8f4e44c616e9ee2495a5bd8ba3e75e2e7916cbdf
danghieuthang <dhthang1998@gmail.com>
2021-12-07 16:15:19 +0700

    Update enctypt and decrypt QR code

--- IMP/IMP.Application/Utils/Utils.cs
+++ IMP/IMP.Application/Utils/Utils.cs
@@ -47,11 +47,12 @@ namespace IMP.Application.Utils
                     encryptString = Convert.ToBase64String(ms.ToArray());
                 }
             }
-            return encryptString;
+            return ConvertStringToHex(encryptString, Encoding.Unicode);
         }
 
         public static string Decrypt(string cipherText)
         {
+            cipherText = ConvertHexToString(cipherText, Encoding.Unicode);
             cipherText = cipherText.Replace(" ", "+");
             byte[] cipherBytes = Convert.FromBase64String(cipherText);
             using (Aes encryptor = Aes.Create())
@@ -72,6 +73,28 @@ namespace IMP.Application.Utils
                 }
             }
             return cipherText;
+        }
+
+
+        static string ConvertStringToHex(String input, System.Text.Encoding encoding)
+        {
+            Byte[] stringBytes = encoding.GetBytes(input);
+            StringBuilder sbBytes = new StringBuilder(stringBytes.Length * 2);
+            foreach (byte b in stringBytes)
+            {
+                sbBytes.AppendFormat("{0:X2}", b);
+            }
+            return sbBytes.ToString();
+        }
+        public static string ConvertHexToString(String hexInput, System.Text.Encoding encoding)
+        {
+            int numberChars = hexInput.Length;
+            byte[] bytes = new byte[numberChars / 2];
+            for (int i = 0; i < numberChars; i += 2)
+            {
+                bytes[i / 2] = Convert.ToByte(hexInput.Substring(i, 2), 16);
+            }
+            return encoding.GetString(bytes);
         }
 
 
