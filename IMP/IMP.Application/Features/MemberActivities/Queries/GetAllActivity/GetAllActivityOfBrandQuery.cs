30d79fd56725b0f8535b98ca89910c2ecfdce62e
danghieuthang <dhthang1998@gmail.com>
2021-12-10 22:08:53 +0700

    Include image in search-all-activity-of-brand

--- IMP/IMP.Application/Features/MemberActivities/Queries/GetAllActivity/GetAllActivityOfBrandQuery.cs
+++ IMP/IMP.Application/Features/MemberActivities/Queries/GetAllActivity/GetAllActivityOfBrandQuery.cs
@@ -45,7 +45,7 @@ namespace IMP.Application.Features.MemberActivities.Queries.GetAllActivity
                             || (request.CampaignIds.Count > 0 && request.CampaignIds.Contains(x.CampaignMember.CampaignId)))
                         && (request.Status.Count == 0
                             || (status.Count > 0 && status.Contains(x.Status))),
-                  include: x => x.Include(ma => ma.CampaignMember).ThenInclude(cm => cm.Campaign).Include(ma => ma.Evidences)
+                  include: x => x.Include(ma => ma.CampaignMember).ThenInclude(cm => cm.Campaign).ThenInclude(c => c.CampaignImages).Include(ma => ma.Evidences)
                     .Include(ma => ma.CampaignMember).ThenInclude(cm => cm.Influencer)
                     .Include(ma => ma.CampaignActivity).ThenInclude(ca => ca.ActivityType),
                   pageIndex: request.PageIndex,
