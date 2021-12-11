7811e834fa397b8326cad695bb6f05a3f7fe744f
danghieuthang <dhthang1998@gmail.com>
2021-12-11 11:34:04 +0700

    Update get voucher code in app

--- IMP/IMP.Application/Features/VoucherCodes/Commands/RequestVoucherCode/RequestVoucherCodeByBiolinkAndCampaignCommand.cs
+++ IMP/IMP.Application/Features/VoucherCodes/Commands/RequestVoucherCode/RequestVoucherCodeByBiolinkAndCampaignCommand.cs
@@ -28,32 +28,85 @@ namespace IMP.Application.Features.VoucherCodes.Commands.RequestVoucherCode
 
             public override async Task<Response<bool>> Handle(RequestVoucherCodeByBiolinkAndCampaignCommand request, CancellationToken cancellationToken)
             {
-                var user = await UnitOfWork.Repository<ApplicationUser>().FindSingleAsync(x => x.Pages.Any(y => y.BioLink.ToLower() == request.Biolink.ToLower()), x => x.VoucherCodeApplicationUsers);
+                var user = await UnitOfWork.Repository<ApplicationUser>().FindSingleAsync(x => x.Pages.Any(y => y.BioLink.ToLower() == request.Biolink.ToLower()));
                 if (user == null)
                 {
                     return new Response<bool>(false);
                 }
 
-                var vouchers = await UnitOfWork.Repository<CampaignVoucher>().GetAll(
+                var campaignMember = await UnitOfWork.Repository<CampaignMember>().FindSingleAsync(x => x.CampaignId == request.CampaignId && x.InfluencerId == user.Id);
+                if (campaignMember == null)
+                {
+                    return new Response<bool>(false);
+                }
+
+                #region check voucher
+                var vouchers = await UnitOfWork.Repository<CampaignVoucher>().FindSingleAsync(
                         predicate: x => x.VoucherId == request.VoucherId
                             && x.CampaignId == request.CampaignId
                             && x.IsBestInfluencerReward == false
-                            && x.IsDefaultReward == false,
-                        selector: x => x.VoucherId).ToListAsync();
+                            && x.IsDefaultReward == false
+                            && (x.Voucher.ToDate == null
+                                || (x.Voucher.ToDate.HasValue && x.Voucher.ToDate.Value.CompareTo(DateTime.UtcNow) > 0) // Get a voucher that hasn't expired yet
+                                ),
+                        include: x => x.Include(y => y.Voucher));
 
-                var voucherCodes = await UnitOfWork.Repository<VoucherCode>().GetAll(
-                        predicate: x => x.CampaignMember.InfluencerId == user.Id
-                            && x.CampaignMember.CampaignId == request.CampaignId
-                            && x.VoucherId == request.VoucherId
-                            && x.QuantityUsed < x.Quantity
-                            && (x.Voucher.ToDate == null || x.Voucher.ToDate > DateTime.UtcNow)
-                            ).Take(1).ToListAsync();
-                if (!voucherCodes.Any())
+                var voucher = vouchers?.Voucher;
+                if (voucher == null)
                 {
-                    return new Response<bool>(message: "Đã hết voucher code.");
+                    return new Response<bool>(message: "Mã giảm giá không tồn tại hoặc đã hết hạn.");
+                }
+                #endregion
+
+                #region get voucher code
+                var voucherCode = await UnitOfWork.Repository<VoucherCode>().FindSingleAsync( // get voucher code that assigned for influencer and can used
+                        predicate: x => x.VoucherId == voucher.Id
+                            && ((x.CampaignMemberId.HasValue && x.CampaignMemberId.Value == campaignMember.Id))
+                            && (
+                                (x.Quantity == 1 && x.QuantityUsed == 0 && (x.Expired == null || (x.Expired.HasValue && x.Expired.Value.CompareTo(DateTime.UtcNow) < 0)))  // Voucher 1 lần sài && chưa ai sài && không bị ai giữ
+                                || (x.Quantity > 1 && x.QuantityUsed < x.Quantity) // Voucher nhiều lần sài và còn sử dụng được
+                               )
+                    );
+
+                if (voucherCode == null)
+                {
+                    // get voucher that not assigned for influencer and can used
+                    voucherCode = await UnitOfWork.Repository<VoucherCode>().FindSingleAsync(
+                       predicate: x => x.VoucherId == voucher.Id
+                           && (x.CampaignMemberId == null)
+                           && (
+                               (x.Quantity == 1 && x.QuantityUsed == 0 && (x.Expired == null || (x.Expired.HasValue && x.Expired.Value.CompareTo(DateTime.UtcNow) < 0)))  // Voucher 1 lần sài && chưa ai sài && không bị ai giữ
+                               || (x.Quantity > 1 && x.QuantityUsed < x.Quantity) // Voucher nhiều lần sài và còn sử dụng được
+                              )
+                   );
                 }
 
-                if (user.VoucherCodeApplicationUsers.Any(x => x.VoucherCodeId == voucherCodes.FirstOrDefault().Id))
+
+                if (voucherCode == null)
+                {
+                    return new Response<bool>(message: "Đã hết mã giảm giá.");
+                }
+                #endregion
+
+                #region update quantity get for voucher influencer
+                voucherCode.QuantityGet++;
+
+                if (voucher.Quantity == 1 && voucher.HoldTime.HasValue)
+                {
+                    voucherCode.Expired = DateTime.UtcNow.Add(voucher.HoldTime.Value);
+                }
+
+                if (voucherCode.CampaignMemberId == null)
+                {
+                    voucherCode.CampaignMemberId = campaignMember.Id;
+
+                }
+
+                UnitOfWork.Repository<VoucherCode>().Update(voucherCode);
+                #endregion
+
+
+                if (user.VoucherCodeApplicationUsers.Any(x => x.VoucherCodeId == voucherCode.Id))
                 {
                     return new Response<bool>(true, "Bạn đã code ở chiến dịch này và vẫn còn có thể sử dụng.");
                 }
@@ -61,7 +114,7 @@ namespace IMP.Application.Features.VoucherCodes.Commands.RequestVoucherCode
                 VoucherCodeApplicationUser voucherCodeApplicationUser = new VoucherCodeApplicationUser
                 {
                     ApplicationUserId = _authenticatedUserService.ApplicationUserId,
-                    VoucherCodeId = voucherCodes.FirstOrDefault().Id,
+                    VoucherCodeId = voucher.Id,
                 };
 
                 UnitOfWork.Repository<VoucherCodeApplicationUser>().Add(voucherCodeApplicationUser);
