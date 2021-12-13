2a0a00b92152c70ddc0adcc57bfe57fb8c0211eb
danghieuthang <dhthang1998@gmail.com>
2021-12-13 20:53:10 +0700

    Update search brand

--- IMP/IMP.Application/Features/Brands/Queries/SearchBrandsQuery.cs
+++ IMP/IMP.Application/Features/Brands/Queries/SearchBrandsQuery.cs
@@ -1,9 +1,11 @@
 ﻿using AutoMapper;
+using IMP.Application.Enums;
 using IMP.Application.Interfaces;
 using IMP.Application.Models;
 using IMP.Application.Models.ViewModels;
 using IMP.Application.Wrappers;
 using IMP.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,6 +17,12 @@ namespace IMP.Application.Features.Brands.Queries
 {
     public class SearchBrandsQuery : PageRequest, IListQuery<BrandViewModel>
     {
+        public SearchBrandsQuery()
+        {
+            Statuses = new List<int>();
+        }
+        [FromQuery(Name = "status")]
+        public List<int> Statuses { get; set; }
         public class SearchBrandsQueryHandler : ListQueryHandler<SearchBrandsQuery, BrandViewModel>
         {
             public SearchBrandsQueryHandler(IUnitOfWork unitOfWork, IMapper mapper) : base(unitOfWork, mapper)
@@ -24,9 +32,10 @@ namespace IMP.Application.Features.Brands.Queries
             public override async Task<Response<IPagedList<BrandViewModel>>> Handle(SearchBrandsQuery request, CancellationToken cancellationToken)
             {
                 var page = await UnitOfWork.Repository<Brand>().GetPagedList(
-                            orderBy: request.OrderField, 
-                            orderByDecensing: request.OrderBy == Enums.OrderBy.DESC, 
-                            pageIndex: request.PageIndex, pageSize: request.PageSize, 
+                            predicate: x => request.Statuses.Count == 0 || request.Statuses.Contains(x.Status),
+                            orderBy: request.OrderField,
+                            orderByDecensing: request.OrderBy == Enums.OrderBy.DESC,
+                            pageIndex: request.PageIndex, pageSize: request.PageSize,
                             cancellationToken: cancellationToken);
                 var pageView = page.ToResponsePagedList<BrandViewModel>(Mapper);
                 return new Response<IPagedList<BrandViewModel>>(pageView);
